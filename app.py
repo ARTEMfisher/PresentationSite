71bc99d2146e2ed4df4e5893a4c9d1b36138235a
ARTEMfisher <artem.b_2005@mail.ru>
2024-04-23 13:50:31 +0300

    requiriments added

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',general='Comming Soon')
+    return render_template('card.html',general='Comming Soon')
 
 @app.route('/about')
 def about():
     return render_template('index.html',general='About green baboon')
 
 app.run(debug=True,host='0.0.0.0',port=80)
+
+
 
 
