08d93ac89a26a1660e7d955c9a8e139e48a897d8
ARTEMfisher <artem.b_2005@mail.ru>
2024-06-28 23:23:31 +0300

    presentation site

--- rundocker.sh
+++ rundocker.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-# Имя Docker-образа
-IMAGE_NAME="my_site_image"
-
-# Порт, который будет использован контейнером
-HOST_PORT=3000
-CONTAINER_PORT=3000
-
-# Строим Docker-образ
-docker build -t $IMAGE_NAME .
-
-# Проверяем, существует ли контейнер с именем my_site_container и удаляем его
-if [ $(docker ps -a -q -f name=my_site_container) ]; then
-    docker rm -f my_site_container
-fi
-
-# Запускаем контейнер
-docker run -d -p $HOST_PORT:$CONTAINER_PORT --name my_site_container $IMAGE_NAME
+docker stop darknesswave
+docker rm darknesswave
+docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=darknesswave $(docker build -q .)
