0a91de35afa92767aaa7ca6a48ddf1f4b46e314c
ARTEMfisher <artem.b_2005@mail.ru>
2024-06-28 23:15:49 +0300

    presentation site

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,19 @@
-docker stop ruina_counter
-docker rm ruina_counter
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 80:80 --name=ruina_counter $(docker build -q .)
+#!/bin/bash
+
+# Имя Docker-образа
+IMAGE_NAME="my_site_image"
+
+# Порт, который будет использован контейнером
+HOST_PORT=3000
+CONTAINER_PORT=3000
+
+# Строим Docker-образ
+docker build -t $IMAGE_NAME .
+
+# Проверяем, существует ли контейнер с именем my_site_container и удаляем его
+if [ $(docker ps -a -q -f name=my_site_container) ]; then
+    docker rm -f my_site_container
+fi
+
+# Запускаем контейнер
+docker run -d -p $HOST_PORT:$CONTAINER_PORT --name my_site_container $IMAGE_NAME
