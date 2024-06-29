05eb36b9a46156ef5fe30cc5db1ad73f0f217c7f
ARTEMfisher <artem.b_2005@mail.ru>
2024-06-29 23:45:20 +0300

    presentation site

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,17 @@
-docker stop server
-docker rm server
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=server $(docker build -q .)
+#!/bin/bash
+
+IMAGE_NAME="my_site_image"
+CONTAINER_NAME="my_site_container"
+HOST_PORT=3000  # Измените на другой свободный порт, например, 3001
+CONTAINER_PORT=3000
+
+# Строим Docker-образ
+docker build -t $IMAGE_NAME .
+
+# Проверяем, существует ли контейнер с именем my_site_container и удаляем его
+if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
+    docker rm -f $CONTAINER_NAME
+fi
+
+# Запускаем контейнер
+docker run -d -p $HOST_PORT:$CONTAINER_PORT --name $CONTAINER_NAME $IMAGE_NAME
