572023998a74bb0781fa1629ce03b642a4954bf4
ARTEMfisher <artem.b_2005@mail.ru>
2024-06-29 12:44:31 +0300

    presentation site

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
-docker stop darknesswave
-docker rm darknesswave
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=darknesswave $(docker build -q .)
+docker stop server
+docker rm server
+docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=server $(docker build -q .)
