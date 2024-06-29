b5d82333edac116ad04f4f502a15ab1ffb1fcf52
ARTEMfisher <artem.b_2005@mail.ru>
2024-06-30 00:38:22 +0300

    presentation site

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,15 @@
-docker stop darknesswave
-docker rm darknesswave
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=darknesswave $(docker build -q .)
-#pipets
+#!/bin/bash
+
+# Определяем имя Docker образа и контейнера
+IMAGE_NAME="darknesswave"
+CONTAINER_NAME="darknesswave"
+
+# Останавливаем и удаляем существующий контейнер
+docker stop $CONTAINER_NAME 2>/dev/null
+docker rm $CONTAINER_NAME 2>/dev/null
+
+# Собираем Docker образ и получаем его ID
+DOCKER_IMAGE_ID=$(docker build -q .)
+
+# Запускаем Docker контейнер
+docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name $CONTAINER_NAME $DOCKER_IMAGE_ID
