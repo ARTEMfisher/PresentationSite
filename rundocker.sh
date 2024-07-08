76f100f3a710361dbb423a3b9fb474d56505cb52
ARTEMfisher <artem.b_2005@mail.ru>
2024-07-08 23:47:40 +0300

    presentation site

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,4 @@
 docker stop darknesswave
 docker rm darknesswave
 IMAGE_ID=$(docker build -q .)
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8081:80 --name=darknesswave $IMAGE_ID
+docker run -it -d -v $(pwd):/opt/app --restart=always -p 80 --name=darknesswave $IMAGE_ID
