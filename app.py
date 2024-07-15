75e59633c7a1491925ce5319992a59c50faeaa49
ARTEMfisher <artem.b_2005@mail.ru>
2024-07-15 21:43:00 +0300

    presentation site

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def log_ip(ip_address):
+    log_entry = {'ip': ip_address}
+    try:
+        with open('ips.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = []
+
+    data.append(log_entry)
+
+    with open('ips.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+
 @app.route('/')
 def index():
+    ip_address = request.remote_addr
+    log_ip(ip_address)
     return render_template('index.html')
 
 @app.errorhandler(404)
