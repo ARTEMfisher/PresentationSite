38750dc7f46048f5d0c8a870847305b80338f24f
ARTEMfisher <artem.b_2005@mail.ru>
2024-07-15 21:51:53 +0300

    presentation site

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def log_ip(ip_address):
-    with open('ips.txt', 'a') as file:
-        file.write(f"{ip_address}\n")
-
 
 
 @app.route('/')
 def index():
-    ip_address = request.remote_addr
-    log_ip(ip_address)
     return render_template('index.html')
 
 @app.errorhandler(404)
